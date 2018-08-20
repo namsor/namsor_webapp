c00855210ce803a18d396fae29f0fa5d4353fafe
HiDoc <fmadura@student.42.fr>
2018-08-20 18:46:30 +0200

    autopush

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-tar --exclude=release --exclude=node_modules --exclude=.git -zcvf ./release/0.0.1/webapp.tar.gz .
+tar --exclude=release --exclude=node_modules --exclude=.git -zuvf ./release/0.0.1/webapp.tar.gz .
 git add *
 git commit -m "autopush"
 git push
