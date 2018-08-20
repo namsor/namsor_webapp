ffca8f7bdaa4a08101a92f2323372d221993e04a
HiDoc <fmadura@student.42.fr>
2018-08-20 18:27:01 +0200

    autopush

--- js/firebase.js
+++ js/firebase.js
@@ -9,41 +9,44 @@ var config = {
 
 firebase.initializeApp(config);
 initApp = function () {
-    var deferred = new Promise(function(resolve, reject){});
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            user.getIdToken().then(function (accessToken) {
-                // Create a request variable and assign a new XMLHttpRequest object to it.
-                var request = new XMLHttpRequest();
-                // Open a new connection, using the GET request on the URL endpoint
-                request.open('GET', '/NamSorAPIv2/api2/json/procureKey/' + accessToken, true);
-                request.onload = function () {
-                    // Begin accessing JSON data here
-                    // Begin accessing JSON data here
-                    var data = JSON.parse(this.response);
-                    console.log(data.api_key);
-                    if (document.getElementById('api_key') != null)
-                      document.getElementById('api_key').textContent = data.api_key;
-                    document.getElementById('signIn').hidden = true;
-                    document.getElementById('navbarDropdownPortfolio').hidden = false;
-                    window.api_key = data.api_key;
-                    console.log('signedin api_key='+window.api_key);
-                }
-                // Send request
-                request.send();
-            });
-        } else {
-            document.getElementById('signIn').hidden = false;
-            document.getElementById('navbarDropdownPortfolio').hidden = true;
-            window.api_key = null;
-            console.log('signedout api_key='+window.api_key)
-        }
-    }, function (error) {
-        console.log(error);
+    var deferred = new Promise(function(resolve, reject){
+      firebase.auth().onAuthStateChanged(function (user) {
+          if (user) {
+              // User is signed in.
+              user.getIdToken().then(function (accessToken) {
+                  // Create a request variable and assign a new XMLHttpRequest object to it.
+                  var request = new XMLHttpRequest();
+                  // Open a new connection, using the GET request on the URL endpoint
+                  request.open('GET', '/NamSorAPIv2/api2/json/procureKey/' + accessToken, true);
+                  request.onload = function () {
+                      // Begin accessing JSON data here
+                      // Begin accessing JSON data here
+                      var data = JSON.parse(this.response);
+                      console.log(data.api_key);
+                      if (document.getElementById('api_key') != null)
+                        document.getElementById('api_key').textContent = data.api_key;
+                      document.getElementById('signIn').hidden = true;
+                      document.getElementById('navbarDropdownPortfolio').hidden = false;
+                      window.api_key = data.api_key;
+                      console.log('signedin api_key='+window.api_key);
+                  }
+                  // Send request
+                  request.send();
+                  resolve("okay");
+              });
+          } else {
+              document.getElementById('signIn').hidden = false;
+              document.getElementById('navbarDropdownPortfolio').hidden = true;
+              window.api_key = null;
+              console.log('signedout api_key='+window.api_key)
+              resolve("okay");
+          }
+      }, function (error) {
+          console.log(error);
+      });
     });
     signOut();
-    return deferred.resolve();
+    return deferred;
 };
 
 var signOut = function () {
