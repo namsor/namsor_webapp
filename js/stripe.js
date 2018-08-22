226e99df5821de930c4d2e8dc92cca03c1a120ab
HiDoc <fmadura@student.42.fr>
2018-08-22 17:08:07 +0200

    autopush

--- js/stripe.js
+++ js/stripe.js
@@ -1,21 +1,16 @@
-firebase.auth().currentUser(function (user) {
-    if (user) {
-        // User is signed in.
-        user.getIdToken().then(function (accessToken) {
-            // Create a request variable and assign a new XMLHttpRequest object to it.
-            var request = new XMLHttpRequest();
-            // Open a new connection, using the GET request on the URL endpoint
-            request.open('GET', '/NamSorAPIv2/api2/json/userInfo/' + accessToken, true);
-            request.onload = function () {
-                // Begin accessing JSON data here
-                // Begin accessing JSON data here
-                var data = JSON.parse(this.response);
-                console.log(data);
-            }
-            // Send request
-            request.send();
-        });
+firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+  // Create a request variable and assign a new XMLHttpRequest object to it.
+  var request = new XMLHttpRequest();
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', '/NamSorAPIv2/api2/json/userInfo/' + accessToken, true);
+  request.onload = function () {
+      // Begin accessing JSON data here
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response);
+      console.log(data);
     }
-    }, function (error) {
-    console.log(error);
+    // Send request
+    request.send();
+}).catch(function(error) {
+  console.log(error);
 });
