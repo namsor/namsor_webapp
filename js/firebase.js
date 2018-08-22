b94ead4599211b8a58d08d4954bf19fa01871a7f
HiDoc <fmadura@student.42.fr>
2018-08-22 17:13:55 +0200

    autopush

--- js/firebase.js
+++ js/firebase.js
@@ -22,14 +22,12 @@ initApp = function () {
                       // Begin accessing JSON data here
                       // Begin accessing JSON data here
                       var data = JSON.parse(this.response);
-                      console.log(data.api_key);
                       if (document.getElementById('api_key') != null)
                         document.getElementById('api_key').textContent = data.api_key;
                       document.getElementById('signIn').hidden = true;
                       document.getElementById('navbarDropdownPortfolio').hidden = false;
                       window.api_key = data.api_key;
-                      console.log('signedin api_key='+window.api_key);
-                  }
+                    }
                   // Send request
                   request.send();
                   resolve("okay");
@@ -38,7 +36,6 @@ initApp = function () {
               document.getElementById('signIn').hidden = false;
               document.getElementById('navbarDropdownPortfolio').hidden = true;
               window.api_key = null;
-              console.log('signedout api_key='+window.api_key)
               resolve("okay");
           }
       }, function (error) {
@@ -53,14 +50,33 @@ var signOut = function () {
   document.getElementById('signOut').addEventListener('click', function (event) {
     firebase.auth().signOut();
     window.api_key = null;
-    console.log('reset api_key='+window.api_key)
     document.getElementById('api_key').textContent = '';
     document.getElementById('signIn').hidden = false;
     document.getElementById('navbarDropdownPortfolio').hidden = true;
     });
 }
 
+var getInfo = function (){
+  firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest();
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', '/NamSorAPIv2/api2/json/userInfo/' + accessToken, true);
+    request.onload = function () {
+        // Begin accessing JSON data here
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+        console.log(data);
+        return data;
+      }
+      // Send request
+      request.send();
+  }).catch(function(error) {
+    console.log(error);
+  });
+}
+
 window.addEventListener('load', function () {
-    initApp();
+    initApp().then(getInfo());
     console.log(window.api_key);
 });
