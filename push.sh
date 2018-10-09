61f194eebb9134a3be20c531a40fd47fe8ab7ca1
HiDoc <fmadura@student.42.fr>
2018-10-09 14:28:00 +0200

    cleaning

--- push.sh
+++
@@ -1,5 +0,0 @@
-tar --exclude=release --exclude=node_modules --exclude=.git -czvf ./release/0.0.1/webapp.tar.gz .
-git add *
-git commit -m "autopush"
-git push
-curl -X GET "http://195.201.247.18:8080/NamSorAPIv2/api2/json/redeployUI" -H "accept: */*" -H "X-API-KEY: 19cc95884c46d1a1e664aa520f7213fc"
