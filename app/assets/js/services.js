373472ffb3ca5a70d9f5df88308cc0814d6c541c
HiDoc <fmadura@student.42.fr>
2018-12-10 15:24:54 +0100

    add currency and resolve padding

--- app/assets/js/services.js
+++ app/assets/js/services.js
@@ -1,30 +1,5 @@
-var request = obj => {
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open(obj.method || "GET", "/NamSorAPIv2/api2/json/" + obj.url, true);
-    if (obj.headers) {
-      Object.keys(obj.headers).forEach(key => {
-        xhr.setRequestHeader(key, obj.headers[key]);
-      });
-    }
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    xhr.onerror = () => reject(xhr.statusText);
-    xhr.send(obj.body);
-  });
-};
-
 var billingCurrencies = request({url : "billingCurrencies"})
 var availablePlans = request({url : "availablePlans"})
-
-billingCurrencies.then( data => {
-  console.log(data)
-});
 
 var currentPlanName = function() {
   return new Promise(function(resolve, reject) {
@@ -37,7 +12,8 @@ var currentPlanName = function() {
   });
 }
 
-var insertServices = function(data, current, signedIn) {
+function dataServicesToHTML(data, current, signedIn, currency)
+{
   let html = '';
   data.plans.forEach( plan => {
     if (plan.planName != "ENTERPRISE") {
@@ -46,26 +22,23 @@ var insertServices = function(data, current, signedIn) {
       <div class="card h-100">
       <h3 class="card-header">${plan.planName}</h3>
       <div class="card-body">
-      <div class="display-4">${plan.price}</div>
+      <div class="display-4">${plan.price} ${currency}</div>
       <div class="font-italic"></div>
       </div>
       <ul class="list-group list-group-flush">
       <li class="list-group-item">Plan quota : ${plan.planQuota}</li>
       <li class="list-group-item">Price overage : ${plan.priceOverage} </li>
       <li class="list-group-item">`;
-      if (signedIn === false)
-      {
+      if (!signedIn) {
         html += `<button class="btn btn-primary">Sign in !</button>`;
-      }
-      else if (plan.planName == current) {
+      } else if (plan.planName == current) {
         html += `<button data-name="${plan.planName}" disabled id="current" class="btn btn-success">Current</button>`
-      }
-      else {
+      } else {
         html += `<button data-name="${plan.planName}"
         data-loading-text="<i class='fa fa-spinner fa-spin'></i> Processing Order"
         class="btn btn-primary">Subscribe!</button>`
       }
-      html +=       `</li>
+      html += `</li>
       </ul>
       </div>
       </div>
@@ -75,6 +48,10 @@ var insertServices = function(data, current, signedIn) {
   return (html);
 }
 
+var insertServices = function(data, current, signedIn, currency) {
+  return dataServicesToHTML(data, current, signedIn, currency);
+}
+
 var insertData = function(prep) {
   availablePlans.then( data => {
     const services = document.getElementById('services')
@@ -82,11 +59,10 @@ var insertData = function(prep) {
     data = JSON.parse(data);
     currentPlanName().then(function(current) {
       current = current.subscription.planName;
-      html = insertServices(data, current, true);
-      services.innerHTML = html;
       getToken().then(function (token) {
         getUsage().then(function(usage){
           usage = JSON.parse(usage);
+          services.innerHTML = insertServices(data, current, true, usage.subscription.currency);
           if (usage.subscription.stripeStatus == 'active') {
             jQuery('#services').find('button').not('#current').click(function (event) {
               let p = "subscribePlan/" + this.dataset.name + "/" + token;
@@ -121,12 +97,16 @@ var insertData = function(prep) {
             );
           }
         });
-      }, function (error) { console.log(error) });
+      }, alertBox(
+          'There is an error occuring during the process, please try again later or contact an administrator',
+          'warning',
+          services
+        )
+      );
     }, function (error) {
-      html = insertServices(data, '', false);
-      services.innerHTML = html;
+      services.innerHTML = insertServices(data, '', false, 'USD');
       jQuery('#services').find('button').each(function (index, button) {
-        button.textContent = 'Sign in';
+        button.textContent = 'Sign in !';
         button.addEventListener('click', function(event){
           document.getElementById('signin').click();
         });
