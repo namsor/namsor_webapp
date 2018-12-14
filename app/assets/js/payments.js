02b1c33e15b40cc15159f6fa9e5c16da44a1b14e
HiDoc <fmadura@student.42.fr>
2018-12-14 18:13:00 +0100

    separation between files

---
+++ app/assets/js/payments.js
@@ -0,0 +1,60 @@
+window.onload = function () {
+    Promise.all([initApp(), getInfo()])
+        .then(data => {
+            appendStripeButton(JSON.parse(data[1]));
+            appendTable();
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+let cTd = (x) => {
+    td = document.createElement('td');
+    td.innerHTML = x;
+    return (td)
+}
+
+let tbody = document.getElementById('cardInfo');
+
+let appendStripeButton = (userInfo) => {
+    var box = document.createElement("script");
+    box.src = "https://checkout.stripe.com/checkout.js";
+    box.type = "text/javascript";
+    box.dataset.email = userInfo.email;
+    box.dataset.key = userInfo.stripePerishableKey;
+    box.dataset.name = "NamSor";
+    box.className = "stripe-button"
+    box.dataset.amount = 0;
+    box.dataset.description = "Namsor API Setup";
+    document.getElementById("stripeForm").appendChild(box);
+}
+
+let appendTable = () => {
+    tokenRequest('paymentInfo').then(datas => {
+        cards = JSON.parse(datas).stripedCards;
+        for (var i = 0; i < cards.length; i++) {
+            let card = cards[i];
+            let tr = document.createElement('tr')
+            if (card.defaultCard)
+                tr.append(cTd('Default'));
+            else {
+                let change = cTd('Change')
+                change.addEventListener('click', function (event) {
+                    tokenRequest(`/api2/json/updatePaymentDefault/${card.sourceId}`)
+                        .then(success => {
+                            tbody.innerHTML = '';
+                            appendTable();
+                        })
+                });
+                tr.append(change);
+            }
+            tr.append(cTd(card.brand));
+            tr.append(cTd('**** '.repeat(3) + card.last4));
+            tr.append(cTd(card.expMonth + "/" + card.expYear));
+            if (card.defaultCard)
+                tr.classList.add('border', 'border-primary')
+            tbody.append(tr);
+        }
+    })
+}
