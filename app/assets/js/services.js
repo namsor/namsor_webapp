215f1d56af756e3e8d17f364c2167cdb27f4e86b
HiDoc <fmadura@student.42.fr>
2018-12-21 16:54:03 +0100

    [regression] data-toggle

--- app/assets/js/services.js
+++ app/assets/js/services.js
@@ -30,13 +30,13 @@ function dataServicesToHTML(data, current, signedIn, currency) {
       <li class="list-group-item">Price overage : ${plan.priceOverage} ${currency}</li>
       <li class="list-group-item">`;
       if (!signedIn) {
-        html += `<button class="btn btn-primary" data-toggle="confirmation" >Sign in !</button>`;
+        html += `<button class="btn btn-primary">Sign in !</button>`;
       } else if (plan.planName == current) {
         html += `<button data-name="${plan.planName}" disabled id="current" class="btn btn-success">Current</button>`
       } else {
         html += `<button data-name="${plan.planName}"
         data-loading-text="<i class='fa fa-spinner fa-spin'></i> Processing Order"
-        class="btn btn-primary">Subscribe!</button>`
+        class="btn btn-primary" data-toggle="confirmation">Subscribe!</button>`
       }
       html += `</li>
       </ul>
