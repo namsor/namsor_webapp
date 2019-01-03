5422acc79f3e907648217447f74fc85fa8a21bd6
HiDoc <fmadura@student.42.fr>
2019-01-03 15:00:48 +0100

    [payments] css change

--- app/assets/js/payments.js
+++ app/assets/js/payments.js
@@ -47,16 +47,18 @@ let appendTable = () => {
         cards = JSON.parse(datas).stripedCards;
         for (var i = 0; i < cards.length; i++) {
             let card = cards[i];
-            let tr = document.createElement('tr')
-            if (card.defaultCard)
-            {
-                let td = (cTd('Default'));
-                td.classList.add('bg-info', 'text-white');
-                tr.append(td);
+            let tr = document.createElement('tr');
+            let btn = document.createElement('button');
+            btn.classList.add('w-100', 'h-100', "border-0");
+            if (card.defaultCard) {
+                btn.innerHTML = "Default";
+                btn.classList.add("btn-info");
+                btn.disabled = true;
             }
             else {
-                let change = cTd('<button class="w-100 h-100 border-0 btn-warning">Change</button>');
-                change.addEventListener('click', function (event) {
+                btn.innerHTML = "Change";
+                btn.classList.add("btn-warning");
+                btn.addEventListener('click', function (event) {
                     tokenRequest(`updatePaymentDefault/${card.sourceId}`)
                         .then(success => {
                             tbody.innerHTML = '';
@@ -65,11 +67,14 @@ let appendTable = () => {
                                 "Your default card has been updated", 
                                 "success", 
                                 document.getElementsByClassName('main-content')[0]
-                                );
+                            );
                         })
                 });
-                tr.append(change);
             }
+            let td = cTd('');
+            td.append(btn);
+            tr.append(td);
+            tr.append(change);
             tr.append(cTd(card.brand));
             tr.append(cTd('**** '.repeat(3) + card.last4));
             tr.append(cTd(card.expMonth + "/" + card.expYear));
