7daa22612c299b41f09d647db1f0392bf9382b2a
HiDoc <fmadura@student.42.fr>
2019-01-04 17:46:17 +0100

    [index] append box and change color

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,15 +1,16 @@
 window.onload = () => {
   let box = document.getElementById('index-right-box');
   initApp().then((success) => {
-    buildCard(true);
+    box.innerHTML = buildCard(true);
   }, (error) => {
-    buildCard(false);
+    box.innerHTML = buildCard(false);
   });
 }
 
 let buildCard = (success) => {
   let amsg = !success ? "Sign up" : "Try it out";
-  let aref = !success ? "sign-in.html" : "apidoc.html"
+  let aref = !success ? "sign-in.html" : "apidoc.html";
+  let aclr = !success ? "primary" : "info";
   let html = `
     <h4 class="card-title p-2">
       Get your API Key
@@ -19,7 +20,7 @@ let buildCard = (success) => {
       <p>FREE TRIAL – NO CREDIT CARD NEEDED.</p>
     </div>
     <div class="p-2">
-    <a href="${aref}" class="btn btn-primary no-mrg-btm">${amsg} !</a>
+    <a href="${aref}" class="btn btn-${aclr} no-mrg-btm">${amsg} !</a>
     </div>
  `;
   return html;
