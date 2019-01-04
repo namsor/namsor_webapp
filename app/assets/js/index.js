50db98e08abda702fd940f3760781154cd584508
HiDoc <fmadura@student.42.fr>
2019-01-04 17:43:18 +0100

    [index] remove sign in

---
+++ app/assets/js/index.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+  let box = document.getElementById('index-right-box');
+  initApp().then((success) => {
+    buildCard(true);
+  }, (error) => {
+    buildCard(false);
+  });
+}
+
+let buildCard = (success) => {
+  let amsg = !success ? "Sign up" : "Try it out";
+  let aref = !success ? "sign-in.html" : "apidoc.html"
+  let html = `
+    <h4 class="card-title p-2">
+      Get your API Key
+    </h4>
+    <div class="card-text mb-auto p-2">
+      <p>The API is free to classify up to 500 names per month (origin, ethnicity) and 5000 names per month (gender).</p>
+      <p>FREE TRIAL – NO CREDIT CARD NEEDED.</p>
+    </div>
+    <div class="p-2">
+    <a href="${aref}" class="btn btn-primary no-mrg-btm">${amsg} !</a>
+    </div>
+ `;
+  return html;
+} 
