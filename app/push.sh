364c7899126fba481ee377ad82f46e304a13c49c
NamSor <elian.carsenat@namsor.com>
2019-05-09 12:49:49 +0200

    removed usersnap, added tag mgr
    removed usersnap, added tag mgr, sitemap.xml and robots.txt

--- app/push.sh
+++ app/push.sh
@@ -2,9 +2,9 @@ git pull
 mv ../bower_components .
 tar --exclude=release --exclude=node_modules --exclude=.git -czvf ../release/0.0.1/webapp.tar.gz \
 META-INF WEB-INF bower_components/**/dist/* assets/*  bower_components/PACE/** bower_components/perfect-scrollbar/** \
-*.html *.js *.map *.css 
+*.html *.js *.map *.css robots.txt sitemap.xml
 mv ./bower_components ../
 git add ../release/
 git commit -m "autopush"
 git push
-curl -X GET "https://v2.namsor.com/NamSorAPIv2/api2/json/redeployUI" -H "accept: */*" -H "X-API-KEY: 19cc95884c46d1a1e664aa520f7213fc"
+curl -X GET "https://v2.namsor.com/NamSorAPIv2/api2/json/redeployUI" -H "accept: */*" -H "X-API-KEY: $1"
