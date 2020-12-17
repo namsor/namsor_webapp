7dab8917a979e547adbb09381bda9364bbe8ec45
CamilleRungette <c.rungette@gmail.com>
2020-12-17 16:39:18 +0100

    log

--- app/assets/csv/js/final.js
+++ app/assets/csv/js/final.js
@@ -315,6 +315,7 @@ const dropzoneComponent = new Dropzone(dropzone, {
     init: function() {
         //------------------------- Lors d'un ajout d'un fichier ------------------//
         this.on("addedfile", function(file) {
+          console.log('file', file);
           console.log(file.type);
           console.log('csvTest: ', ["text/csv"].includes(file.type), "text/csv" === file.type, typeof file.type)
           console.log('txtTest: ', ["text/plain"].includes(file.type), "text/plain" === file.type, typeof file.type)
